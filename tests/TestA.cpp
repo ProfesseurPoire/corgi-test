26606c839360404f2688f4d391fdb186e3e3b160
ProfesseurPoire <guyot.ced@gmail.com>
2023-02-28 11:36:23 +0100

    adds warnings

--- tests/TestA.cpp
+++ tests/TestA.cpp
@@ -2,19 +2,17 @@
 
 using namespace corgi::test;
 
-TEST(TestA,Check)
+TEST(TestA, Check)
 {
     assert_that(true, corgi::test::equals(true));
 }
 
-TEST(TestA,CheckEquals)
+TEST(TestA, CheckEquals)
 {
-    int val = 10;
-
     assert_that(true, corgi::test::equals(true));
 }
 
-TEST(TestA,CheckNonEquals)
+TEST(TestA, CheckNonEquals)
 {
     assert_that(10, corgi::test::non_equals(5));
 }
